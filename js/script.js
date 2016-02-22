c9d93ead709de8b338aca0e3a3debc800e0125fc
icapa9 <icapa.rescuettp@gmail.com>
2016-02-22 13:20:38 +0300

    ап

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var link = document.querySelector (".show-form");
-var form = document.querySelector (".hotels-search");
-var close = document.querySelector (".show-form");
+var link = document.querySelector(".show-form");
+var form = document.querySelector(".hotels-search");
+var close = document.querySelector(".show-form");
 
-link.addEventListener ("click", function (event) {
-  event.preventDefault ();
-  form.classList.add ("hotels-search-show");
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    form.classList.add("hotels-search-show");
 });
 
-close.addEventListener ("click", function (event) {
-  event.preventDefault ();
-  form.classList.toggle ("hotels-search-hide");
+close.addEventListener("click", function(event) {
+    event.preventDefault();
+    form.classList.toggle("hotels-search-hide");
 });
